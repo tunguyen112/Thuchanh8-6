c817f56bf9601221de07d99fcf3cb11fdfdd5b4c
tunguyen112 <152349495+tunguyen112@users.noreply.github.com>
2023-12-08 09:15:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import streamlit as st
+import pickle as pkl
+import numpy as np
+from PIL import Image
 
+class_list = {'0': 'Male', '1': 'Female'}
+st.title("Predict Genders for Vietnamese Names")
+
+input_ec = open('ec_vinames.pkl', 'rb')
+encoder = pkl.load(input_ec)
+
+input_md = open('lrc_vinames.pkl', 'rb')
+model = pkl.load(input_md)
+
+st.header('Write a feedback')
+txt = st.text_area('', '')
+
+if txt != '':
+  if st.button('Predict'):
+    feature_vector = encoder.transform([txt])
+    label = str((model.predict(feature_vector))[0])
+
+    st.header('Result')
+    st.text(class_list[label])
